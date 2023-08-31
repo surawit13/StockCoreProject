58565cc5ed0e6299ecd0aad88f0678f9e6e53861
surawit13 <surawit.jultura@gmail.com>
2023-08-31 14:05:50 +0700

    commit add DTO request and response

---
+++ StockCore/DTOs/Product/ProductRequest.cs
@@ -0,0 +1,22 @@
+﻿using StockCore.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockCore.DTOs.Product
+{
+    public class ProductRequest
+    {
+        //add ? mean can be null
+        public int? ProductId { get; set; }
+        [Required]
+        [MaxLength(150,ErrorMessage ="Name, Maximun length 150")]
+        public string Name { get; set; } = null!;
+        [Range(0,1_000)]
+        public int Stock { get; set; }
+        [Range(0, 1_000_000)]
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        //add ? mean can be null
+        public List<IFormFile>? FormFiles { get; set; }
+
+    }
+}
---
+++ StockCore/DTOs/Product/ProductResponse.cs
@@ -0,0 +1,28 @@
+﻿using StockCore.Entities;
+namespace StockCore.DTOs.Product
+{
+    public class ProductResponse
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Image { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public static ProductResponse FromProduct(StockCore.Entities.Product product){
+
+            return new ProductResponse
+            {
+                ProductId = product.ProductId,
+                Name = product.Name,
+                Price = product.Price,
+                Image = product.Image,
+                Stock = product.Stock,
+                CategoryName = product.Category.Name,
+
+            };
+        }
+    }
+}
